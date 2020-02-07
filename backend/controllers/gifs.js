23d79822e3ae372720bea7c5c6abb695372e6f1c
SRBond <skyler.bond7@gmail.com>
2020-02-07 10:13:55 -0800

    Return all gifs from put/post/delete

--- backend/controllers/gifs.js
+++ backend/controllers/gifs.js
@@ -13,17 +13,29 @@ router.get("/", (req, res) => {
 //update one gif
 router.put("/:id", (req, res) => {
 	console.log(req.body);
-	Gif.findByIdAndUpdate(req.params.id, req.body).then(() => {});
+	Gif.findByIdAndUpdate(req.params.id, req.body).then(() => {
+		Gif.find({}).then(gifs => {
+			res.json(gifs);
+		});
+	});
 });
 
 //add one gif
 router.post("/", (req, res) => {
-	Gif.create(req.body);
+	Gif.create(req.body).then(() => {
+		Gif.find({}).then(gifs => {
+			res.json(gifs);
+		});
+	});
 });
 
 //delete one gif
 router.delete("/:id", (req, res) => {
-	Gif.findOneAndRemove({ _id: req.params.id }).then(() => {});
+	Gif.findOneAndRemove({ _id: req.params.id }).then(() => {
+		Gif.find({}).then(gifs => {
+			res.json(gifs);
+		});
+	});
 });
 
 module.exports = router;
